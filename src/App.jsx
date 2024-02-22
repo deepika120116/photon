73e7736816f75f13573e7a3dbf7debabcf144067
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-22 10:01:14 -0500

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,65 @@ import {useState,useEffect} from 'react';
 
 export default function App() {
  const [data,setData]=useState([]);
- useEffect(()=>{
-  const abortController=new AbortController();
-  const signal=abortController.signal;
-const dataFetch=(async()=>{
-  const response= await fetch("https://data.cityofnewyork.us/resource/s3k6-pzi2.json",{signal});
-  try{
+ const [filteredData,setFilteredData]=useState(data);
+      useEffect(()=>{
+        const abortController=new AbortController();
+        const signal=abortController.signal;
+        const dataFetch=(async()=>{
+        const response= await fetch("https://data.cityofnewyork.us/resource/s3k6-pzi2.json",{signal});
+        try{
 
-    if(response.ok){
-      const data=await response.json();
-      setData(data);
-    }
-    
-  }catch(err){
-    if(err.name==="AbortError")
-      console.error("Sorry the fetch was aborted");
-    console.error("Something went wrong",err.message);
-  }
-});
-dataFetch();
-return()=>{
-  setTimeout(()=>abortController.abort(),5000);
-}
- },[]);
- 
+          if(response.ok){
+            const data=await response.json();
+            setData(data);
+          }
+          
+        }catch(err){
+          if(err.name==="AbortError")
+            console.error("Sorry the fetch was aborted");
+          console.error("Something went wrong",err.message);
+        }
+      });
+      dataFetch();
+      return()=>{
+        setTimeout(()=>abortController.abort(),5000);
+      }
+      },[]);
+
+      const handleChange=(e)=>{
+        let inputVal=e.target.value;
+        const searchData=data.filter(schoolInfo=>{
+          return (inputVal.includes(schoolInfo.school_name))
+        });
+      console.log(searchData);
+
+      setFilteredData(searchData);
+      }
+  
   return (
     <>
     <h2>School List</h2>
-    <ul>
+
+    {/* <input type="list" list="schools" placeholder='Search for school...' onChange={handleChange}/>
+    <datalist id="schools">
+      {filteredData.length>0 && filteredData.map(schoolData=>{
+        return <option key={schoolData.dbn} value={schoolData.school_name}>{schoolData.school_name}</option>
+      })}
+      </datalist> */}
+
+{/* <pre>{JSON.stringify(filteredData,null,2)}</pre> */}
+      
+<select name="search"   onChange={handleChange}>
+{data.length>0 && data.map(schoolData=>{
+        return <option key={schoolData.dbn} value={schoolData.school_name}>{schoolData.school_name} </option>
+      })}
+</select>
+
+   {filteredData.length>0 && filteredData.map(data=>{
+    return<p key={data.dbn}>{data.overview_paragraph}</p>
+   })}
+
+    {/* <ul>
 
     {data.length>0 && data.map(school=>{
       return <li key={school.dbn}>{school.school_name}
@@ -38,7 +69,7 @@ return()=>{
     })}
      </ul>
 
-    {/* <pre>{JSON.stringify(data,null,2)}</pre> */}
+    <pre>{JSON.stringify(data,null,2)}</pre> */}
       
     </>
   );
