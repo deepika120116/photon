a5bb82cb6da40735f34b52267be444f619a8baad
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-22 10:03:33 -0500

    final commit without any comments

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,6 @@ export default function App() {
         const searchData=data.filter(schoolInfo=>{
           return (inputVal.includes(schoolInfo.school_name))
         });
-      console.log(searchData);
 
       setFilteredData(searchData);
       }
@@ -42,34 +41,18 @@ export default function App() {
     <>
     <h2>School List</h2>
 
-    {/* <input type="list" list="schools" placeholder='Search for school...' onChange={handleChange}/>
-    <datalist id="schools">
-      {filteredData.length>0 && filteredData.map(schoolData=>{
-        return <option key={schoolData.dbn} value={schoolData.school_name}>{schoolData.school_name}</option>
-      })}
-      </datalist> */}
-
-{/* <pre>{JSON.stringify(filteredData,null,2)}</pre> */}
       
-<select name="search"   onChange={handleChange}>
-{data.length>0 && data.map(schoolData=>{
-        return <option key={schoolData.dbn} value={schoolData.school_name}>{schoolData.school_name} </option>
-      })}
-</select>
+    <select name="search"   onChange={handleChange}>
+    {data.length>0 && data.map(schoolData=>{
+            return <option key={schoolData.dbn} value={schoolData.school_name}>{schoolData.school_name} </option>
+          })}
+    </select>
 
    {filteredData.length>0 && filteredData.map(data=>{
     return<p key={data.dbn}>{data.overview_paragraph}</p>
    })}
 
-    {/* <ul>
-
-    {data.length>0 && data.map(school=>{
-      return <li key={school.dbn}>{school.school_name}
-      <p key={school.dbn}>{school.overview_paragraph}</p></li>
-    })}
-     </ul>
-
-    <pre>{JSON.stringify(data,null,2)}</pre> */}
+  
       
     </>
   );
